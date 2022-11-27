57c1eeb1c890939b42db890b258c85512846f883
0xjhtdm <zjhsdtc@gmail.com>
2022-11-28 01:47:06 +0800

    feat: build cache

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,6 @@ export const DEBUG_NAMESPACE = "hardhat:plugin:fe";
 
 export const GITHUB_RELEASES_URL =
   "https://api.github.com/repos/ethereum/fe/releases?per_page=100";
+
+export const CACHE_FORMAT_VERSION = "hh-fe-cache-1";
+export const FE_FILES_CACHE_FILENAME = "fe-files-cache.json";
